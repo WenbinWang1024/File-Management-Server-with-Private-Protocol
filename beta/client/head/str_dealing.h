62f5f7f8852d22e7a17036c1cc637331c37087cc
LaChimere <chingkun_lin@126.com>
2020-08-04 10:02:23 +0800

    delete old

--- beta/client/head/str_dealing.h
+++
@@ -1,6 +0,0 @@
-#ifndef STR_DEALING_H
-#define STR_DEALING_H
-
-char * pretreat_str(char * s, int len);
-
-#endif // STR_DEALING_H
--- beta/server/head/commands.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-#include "includings.h"
-
-int cmd_pwd(int client_fd);
-
-#endif
