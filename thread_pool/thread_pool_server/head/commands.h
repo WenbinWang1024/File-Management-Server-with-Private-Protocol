fc83649107ef02badb71ac6d5731da6abe5cee83
WenbinWang1024 <64248506+WenbinWang1024@users.noreply.github.com>
2020-08-06 13:51:17 +0800

    20200806
    wwb

--- thread_pool/thread_pool_server/head/commands.h
+++ thread_pool/thread_pool_server/head/commands.h
@@ -18,5 +18,6 @@ int cmd_cd(int fd, char * cmd, char * path);
 int cmd_ls(int fd, char * cmd, char * path);
 
 int cmd_pwd(int fd, char * path);
+int cmd_gets(int cfd, char * cmd);
 
 #endif // COMMANDS_H
